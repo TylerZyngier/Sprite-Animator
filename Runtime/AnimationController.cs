05e41b5ef94407784e1c5d5b14421c0858226150
Tyler Zyngier <tyler.zyngier@gmail.com>
2023-06-06 21:53:54 -0400

    Update AnimationController.cs

--- Runtime/AnimationController.cs
+++ Runtime/AnimationController.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 
-namespace SpriteAnimator
+namespace SpriteAnimation
 {
     [CreateAssetMenu(fileName = "NewAnimationController", menuName = "Sprite Animator/Animation Controller (Currently not used)", order = 0)]
     public class AnimationController : ScriptableObject
     {
-        public AnimationClip[] animationClips;
+        public SpriteAnimationClip[] animationClips;
     }
 }
